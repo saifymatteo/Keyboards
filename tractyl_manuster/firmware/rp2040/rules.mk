04239b8c8dabc26e857ae52aac4f868061b61b45
Saiful Mashuri <github@saifulmashuri.com>
2025-10-23 22:39:23 +0800

    fix(tractyl): move rules.mk for RP2040 only

---
+++ tractyl_manuster/firmware/rp2040/rules.mk
@@ -0,0 +1,29 @@
+# Bootloader
+BOOTLOADER = rp2040
+
+# Driver
+SERIAL_DRIVER = vendor
+I2C_DRIVER_REQUIRED = yes
+SPI_DRIVER_REQUIRED = yes
+
+# Deferred Callback
+DEFERRED_EXEC_ENABLE = yes
+
+# Audio
+AUDIO_ENABLE = yes
+AUDIO_DRIVER = pwm_hardware
+
+# Encoders
+ENCODER_ENABLE = yes
+
+# OLED
+OLED_ENABLE = yes
+OLED_DRIVER = ssd1306
+OLED_TRANSPORT = i2c
+
+# WPM
+WPM_ENABLE = yes
+
+# Debug
+CONSOLE_ENABLE = yes
+KEYCODE_STRING_ENABLE = yes
--- tractyl_manuster/firmware/rules.mk
+++ tractyl_manuster/firmware/rules.mk
@@ -1,35 +1,5 @@
-# Bootloader
-BOOTLOADER = rp2040
-
-# Driver
-SERIAL_DRIVER = vendor
-I2C_DRIVER_REQUIRED = yes
-SPI_DRIVER_REQUIRED = yes
-
 # OS Detection
 OS_DETECTION_ENABLE = yes
 
-# Deferred Callback
-DEFERRED_EXEC_ENABLE = yes
-
-# Audio
-AUDIO_ENABLE = yes
-AUDIO_DRIVER = pwm_hardware
-
 # Trackball
 POINTING_DEVICE_DRIVER = pmw3360
-
-# Encoders
-ENCODER_ENABLE = yes
-
-# OLED
-OLED_ENABLE = yes
-OLED_DRIVER = ssd1306
-OLED_TRANSPORT = i2c
-
-# WPM
-WPM_ENABLE = yes
-
-# Debug
-CONSOLE_ENABLE = yes
-KEYCODE_STRING_ENABLE = yes
