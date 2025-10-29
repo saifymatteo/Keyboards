3762b021c730e0a41dd47021369555932064aa7a
Saiful Mashuri <github@saifulmashuri.com>
2025-10-30 00:49:11 +0800

    refactor(viktorpad): align keymap with comments

--- viktorpad/firmware/keymaps/vial_matteo/keymap.c
+++ viktorpad/firmware/keymaps/vial_matteo/keymap.c
@@ -28,11 +28,32 @@ enum custom_keycodes {
     ALT_GUI = SAFE_RANGE,
 };
 
-KEYCODE_STRING_NAMES_USER(KEYCODE_STRING_NAME(ALT_GUI), KEYCODE_STRING_NAME(KC_APP), KEYCODE_STRING_NAME(KC_MUTE), KEYCODE_STRING_NAME(DB_TOGG));
+KEYCODE_STRING_NAMES_USER(        //
+    KEYCODE_STRING_NAME(ALT_GUI), //
+    KEYCODE_STRING_NAME(KC_APP),  //
+    KEYCODE_STRING_NAME(KC_MUTE), //
+    KEYCODE_STRING_NAME(DB_TOGG)  //
+);
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] = LAYOUT(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_DEL, KC_TAB, KC_Q, KC_W, KC_F, KC_P, KC_B, KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_BSLS, KC_BSPC, KC_A, KC_R, KC_S, KC_T, KC_G, KC_M, KC_N, KC_E, KC_I, KC_O, KC_QUOT, KC_APP, KC_Z, KC_X, KC_C, KC_D, KC_V, KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH, DF(1), KC_LCTL, KC_LGUI, KC_LSFT, KC_SPC, LALT_T(KC_ENT), TT(2), TT(2), RALT_T(KC_ENT), KC_SPC, KC_RSFT, KC_HOME, KC_END, ALT_GUI),
-                                                              [1] = LAYOUT(KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_TRNS, KC_TRNS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_TRNS, KC_TRNS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, DF(0), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
-                                                              [2] = LAYOUT(QK_REBOOT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_INS, KC_TRNS, KC_TRNS, KC_TRNS, QK_BOOTLOADER, KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_TRNS, KC_GRV, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_TRNS, KC_TRNS, KC_F5, KC_F6, KC_F7, KC_F8, KC_TRNS, ALT_GUI, KC_LEFT, KC_UP, KC_DOWN, KC_RGHT, KC_TRNS, EE_CLR, KC_F9, KC_F10, KC_F11, KC_F12, AU_TOGG, KC_TILD, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, DB_TOGG, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE)};
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = //
+    {[0] = LAYOUT(                                           //
+         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        //
+         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        //
+         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                 //
+         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS         //
+         ),
+     [1] = LAYOUT(                                    //
+         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, //
+         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, //
+         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          //
+         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS  //
+         ),
+     [2] = LAYOUT(                                    //
+         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, //
+         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, //
+         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          //
+         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS  //
+         )};
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     oled_clear();
@@ -42,10 +63,9 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 // ENCODER --------------------------------------------------------------
 
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [0] = {ENCODER_CCW_CW(MS_WHLU, MS_WHLD), ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-    [1] = {ENCODER_CCW_CW(UG_HUED, UG_HUEU), ENCODER_CCW_CW(UG_SATD, UG_SATU)},
-    [2] = {ENCODER_CCW_CW(UG_VALD, UG_VALU), ENCODER_CCW_CW(UG_SPDD, UG_SPDU)},
-    [3] = {ENCODER_CCW_CW(UG_PREV, UG_NEXT), ENCODER_CCW_CW(KC_RIGHT, KC_LEFT)},
+    [0] = {ENCODER_CCW_CW(MS_WHLU, MS_WHLD), ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+    [1] = {ENCODER_CCW_CW(UG_HUED, UG_HUEU), ENCODER_CCW_CW(UG_SATD, UG_SATU), ENCODER_CCW_CW(UG_SATD, UG_SATU)},
+    [2] = {ENCODER_CCW_CW(UG_VALD, UG_VALU), ENCODER_CCW_CW(UG_SPDD, UG_SPDU), ENCODER_CCW_CW(UG_SPDD, UG_SPDU)},
 };
 
 // Alt (Windows) / Command (MacOS)
