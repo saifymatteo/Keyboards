76e07004cda0ea4c1eec32f4731cb043097d953f
Saiful Mashuri <github@saifulmashuri.com>
2025-11-13 00:33:35 +0800

    feat(viktorpad): add base firmware

---
+++ viktorpad/v3/firmware/config.h
@@ -0,0 +1,44 @@
+/*
+Copyright 2025 saifymatteo <dev@saifulmashuri.com>
+*/
+
+#pragma once
+
+// Dynamic Keymap
+#define DYNAMIC_KEYMAP_LAYER_COUNT 4
+
+// For OS Detection | helps with detection issue with KVM device
+#define OS_DETECTION_KEYBOARD_RESET
+
+// Tapping Toggle feature TT(layer)
+#define TAPPING_TOGGLE 3
+
+// Tapping Term
+#define TAPPING_TERM 160
+
+// I2C configuration (for OLED)
+#define I2C1_SCL_PIN D0
+#define I2C1_SDA_PIN D1
+
+// OLED configuration
+#define OLED_BRIGHTNESS 255 // from 0 to 255
+#define OLED_DISPLAY_WIDTH 128
+#define OLED_DISPLAY_HEIGHT 64
+
+// Encoders
+#define ENCODER_A_PINS {B4, B4, B4, B4}
+#define ENCODER_B_PINS {B1, B3, B2, B6}
+#define ENCODER_RESOLUTION 4
+
+// Save memory
+#define LAYER_STATE_8BIT
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
+#define NO_ACTION_ONESHOT
+#define DISABLE_LEADER
+#ifndef NO_DEBUG
+#define NO_DEBUG
+#endif // !NO_DEBUG
+#if !defined(NO_PRINT) && !defined(CONSOLE_ENABLE)
+#define NO_PRINT
+#endif // !NO_PRINT
---
+++ viktorpad/v3/firmware/rules.mk
@@ -0,0 +1,19 @@
+# Bootloader
+BOOTLOADER = caterina
+
+# Driver
+I2C_DRIVER_REQUIRED = yes
+
+# OS Detection
+OS_DETECTION_ENABLE = yes
+
+# Encoders
+ENCODER_ENABLE = yes
+
+# Mouse Key
+MOUSEKEY_ENABLE = yes
+
+# OLED
+OLED_ENABLE = yes
+OLED_DRIVER = ssd1306
+OLED_TRANSPORT = i2c
