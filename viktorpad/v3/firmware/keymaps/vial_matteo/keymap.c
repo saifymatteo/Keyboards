312c3c1db418d639d37ba820222b37ede4009894
Saiful Mashuri <dev@saifulmashuri.com>
2025-12-01 11:38:11 +0800

    [spender][tractyl][viktorpad] fix: use PDF for persistent default layer

--- viktorpad/v3/firmware/keymaps/vial_matteo/keymap.c
+++ viktorpad/v3/firmware/keymaps/vial_matteo/keymap.c
@@ -35,25 +35,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = //
     {
         [0] = LAYOUT(                                       //
             KC_F22, KC_F23, KC_F24, LCTL(KC_0),             //
-            KC_F19, KC_F20, KC_F21, DF(1),                  //
+            KC_F19, KC_F20, KC_F21, PDF(1),                  //
             KC_F16, KC_F17, KC_F18, KC_TRNS,                //
             LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), KC_TRNS     //
             ),                                              //
         [1] = LAYOUT(                                       //
             KC_KP_7, KC_KP_8, KC_KP_9, KC_TRNS,             //
-            KC_KP_4, KC_KP_5, KC_KP_6, DF(2),               //
+            KC_KP_4, KC_KP_5, KC_KP_6, PDF(2),               //
             KC_KP_1, KC_KP_2, KC_KP_3, KC_MEDIA_PLAY_PAUSE, //
             KC_KP_DOT, KC_KP_0, KC_KP_ENTER, KC_MUTE        //
             ),                                              //
         [2] = LAYOUT(                                       //
             KC_TAB, KC_W, KC_Y, KC_TRNS,                    //
-            KC_A, KC_S, KC_D, DF(3),                        //
+            KC_A, KC_S, KC_D, PDF(3),                        //
             KC_LSFT, KC_X, KC_C, KC_MEDIA_PLAY_PAUSE,       //
             KC_LCTL, KC_LALT, KC_SPC, KC_MUTE               //
             ),                                              //
         [3] = LAYOUT(                                       //
             MS_BTN4, MS_BTN3, MS_BTN5, OS_SWITCH_KC,        //
-            MS_BTN1, MS_UP, MS_BTN2, DF(0),                 //
+            MS_BTN1, MS_UP, MS_BTN2, PDF(0),                 //
             MS_LEFT, MS_DOWN, MS_RGHT, ZOOM_KC,             //
             MS_WHLU, KC_LSFT, MS_WHLD, ALT_GUI_KC           //
             ),                                              //
