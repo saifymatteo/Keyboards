c2ba09d1a18f89698291b5453056cbce40239759
Saiful Mashuri <github@saifulmashuri.com>
2025-12-09 01:16:40 +0800

    [viktorpad] fix: temporary use PDF instead LAYER_DOWN_KC

--- viktorpad/firmware/keymaps/vial_matteo/keymap.c
+++ viktorpad/firmware/keymaps/vial_matteo/keymap.c
@@ -56,42 +56,42 @@ void suspend_power_down_user(void) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = //
     {
-        [0] = LAYOUT(                                             // Default
-            KC_F22, KC_F23, KC_F24, LAYER_DOWN_KC,                //
-            KC_F19, KC_F20, KC_F21, LAYER_UP_KC,                  //
-            KC_F16, KC_F17, KC_F18, LCTL(KC_0),                   //
-            LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), KC_TRNS           //
-            ),                                                    //
-        [1] = LAYOUT(                                             // Numpad
-            KC_KP_7, KC_KP_8, KC_KP_9, LAYER_DOWN_KC,             //
-            KC_KP_4, KC_KP_5, KC_KP_6, LAYER_UP_KC,               //
-            KC_KP_1, KC_KP_2, KC_KP_3, KC_MEDIA_PLAY_PAUSE,       //
-            KC_KP_DOT, KC_KP_0, KC_KP_ENTER, KC_MUTE              //
-            ),                                                    //
-        [2] = LAYOUT(                                             // WASD
-            KC_TAB, KC_W, KC_Y, LAYER_DOWN_KC,                    //
-            KC_A, KC_S, KC_D, LAYER_UP_KC,                        //
-            KC_LSFT, KC_X, KC_C, KC_MEDIA_PLAY_PAUSE,             //
-            KC_LCTL, KC_LALT, KC_SPC, KC_MUTE                     //
-            ),                                                    //
-        [3] = LAYOUT(                                             // Mouse
-            MS_BTN4, MS_BTN3, MS_BTN5, LAYER_DOWN_KC,             //
-            MS_BTN1, MS_UP, MS_BTN2, LAYER_UP_KC,                 //
-            MS_LEFT, MS_DOWN, MS_RGHT, ZOOM_KC,                   //
-            MS_WHLU, KC_LSFT, MS_WHLD, ALT_GUI_KC                 //
-            ),                                                    //
-        [4] = LAYOUT(                                             // T9 Keypad
-            TD(TD_1_KC), TD(TD_2_KC), TD(TD_3_KC), LAYER_DOWN_KC, //
-            TD(TD_4_KC), TD(TD_5_KC), TD(TD_6_KC), LAYER_UP_KC,   //
-            TD(TD_7_KC), TD(TD_8_KC), TD(TD_9_KC), KC_TRNS,       //
-            KC_LSFT, TD(TD_0_KC), KC_ENT, KC_TRNS                 //
-            ),                                                    //
-        [5] = LAYOUT(                                             // Settings
-            QK_RBT, EE_CLR, QK_BOOT, LAYER_DOWN_KC,               //
-            AU_TOGG, OS_SWITCH_KC, HAPTIC_TOGGLE_KC, LAYER_UP_KC, //
-            UG_SATU, UG_HUEU, UG_NEXT, VK_TOGG,                   //
-            UG_SATD, UG_HUED, UG_PREV, UG_TOGG                    //
-            ),                                                    //
+        [0] = LAYOUT(                                        // Default
+            KC_F22, KC_F23, KC_F24, PDF(5),                  //
+            KC_F19, KC_F20, KC_F21, PDF(1),                  //
+            KC_F16, KC_F17, KC_F18, LCTL(KC_0),              //
+            LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), KC_TRNS      //
+            ),                                               //
+        [1] = LAYOUT(                                        // Numpad
+            KC_KP_7, KC_KP_8, KC_KP_9, PDF(0),               //
+            KC_KP_4, KC_KP_5, KC_KP_6, PDF(2),               //
+            KC_KP_1, KC_KP_2, KC_KP_3, KC_MEDIA_PLAY_PAUSE,  //
+            KC_KP_DOT, KC_KP_0, KC_KP_ENTER, KC_MUTE         //
+            ),                                               //
+        [2] = LAYOUT(                                        // WASD
+            KC_TAB, KC_W, KC_Y, PDF(1),                      //
+            KC_A, KC_S, KC_D, PDF(3),                        //
+            KC_LSFT, KC_X, KC_C, KC_MEDIA_PLAY_PAUSE,        //
+            KC_LCTL, KC_LALT, KC_SPC, KC_MUTE                //
+            ),                                               //
+        [3] = LAYOUT(                                        // Mouse
+            MS_BTN4, MS_BTN3, MS_BTN5, PDF(2),               //
+            MS_BTN1, MS_UP, MS_BTN2, PDF(4),                 //
+            MS_LEFT, MS_DOWN, MS_RGHT, ZOOM_KC,              //
+            MS_WHLU, KC_LSFT, MS_WHLD, ALT_GUI_KC            //
+            ),                                               //
+        [4] = LAYOUT(                                        // T9 Keypad
+            TD(TD_1_KC), TD(TD_2_KC), TD(TD_3_KC), PDF(3),   //
+            TD(TD_4_KC), TD(TD_5_KC), TD(TD_6_KC), PDF(5),   //
+            TD(TD_7_KC), TD(TD_8_KC), TD(TD_9_KC), KC_TRNS,  //
+            KC_LSFT, TD(TD_0_KC), KC_ENT, KC_TRNS            //
+            ),                                               //
+        [5] = LAYOUT(                                        // Settings
+            QK_RBT, EE_CLR, QK_BOOT, PDF(4),                 //
+            AU_TOGG, OS_SWITCH_KC, HAPTIC_TOGGLE_KC, PDF(0), //
+            UG_SATU, UG_HUEU, UG_NEXT, VK_TOGG,              //
+            UG_SATD, UG_HUED, UG_PREV, UG_TOGG               //
+            ),                                               //
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
